1456d644eaea04b7bb9ebc97fa2a592edc3b7d68
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-09-09 19:47:59 -0300

    adicionando o header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./components/header/header.css">
+  <link rel="stylesheet" href="index.css">
+  <title>Catarino Veículos</title>
+</head>
+
+<body>
+  <header class="container_header">
+    <?php
+    include "./components/header/header.php";
+    ?>
+  </header>
+
+</body>
+
+</html>
