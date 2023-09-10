93a9fc78a9d3ae9bdc6fdf4f7a10f69cfee32c27
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-09-10 11:32:41 -0300

    adicionando o slider

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./components/header/header.css">
+  <link rel="stylesheet" href="./components/slider/slider.css">
   <link rel="stylesheet" href="index.css">
   <title>Catarino Veículos</title>
 </head>
@@ -15,6 +16,17 @@
     include "./components/header/header.php";
     ?>
   </header>
+  <main>
+
+    <section class="container_slider">
+      <?php
+      include "./components/slider/slider.php";
+      ?>
+    </section>
+
+  </main>
+
+  <script src="script.js"></script>
 
 </body>
 
